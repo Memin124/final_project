04a3ae2046427536b8c345afb7090304b7de8fea
yzheng40 <124059386+yzheng40@users.noreply.github.com>
2023-06-13 22:34:58 -0400

    Update FaceNetModel.java

--- finalfolder/FaceNetModel.java
+++ finalfolder/FaceNetModel.java
@@ -13,6 +13,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.IOException;
 import java.nio.FloatBuffer;
+import java.nio.ByteBuffer;
 import java.io.File;
 import java.util.Scanner;
 import java.util.*;
@@ -29,15 +30,17 @@ public class FaceNetModel {
   }
 
   public void loadModel() {
-    try (Graph graph = new Graph()) {
+    try {
+        // Initialize the graph
+        this.graph = new Graph();
+
         // Import the graph definition file (the .pb file)
         byte[] graphBytes = Files.readAllBytes(Paths.get(modelPath));
         graph.importGraphDef(graphBytes);
 
         // Initialize a new TensorFlow session using the imported graph
-        try (Session session = new Session(graph)) {
-            this.session = session;
-        }
+        this.session = new Session(graph);
+
     } catch (IOException e) {
         System.out.println("Failed to load model from path: " + modelPath);
         e.printStackTrace();
@@ -47,31 +50,38 @@ public class FaceNetModel {
 
 
 
-  public float[] getFaceEmbedding(Mat face) {
-    Mat mat = normalizeImage(face);
-    float[][] flattenedMat = new float[(int) mat.total()][mat.channels()];
-    mat.get(0, 0, flattenedMat[0]);
 
-    try (Tensor<Float> tensor = Tensor.create(new long[] {1, 160, 160, 3}, FloatBuffer.wrap(flattenedMat[0]))) {
-      Tensor<Float> output = session.runner()
-                                    .feed("input_1", tensor)
-                                    .fetch("Bottleneck_BatchNorm/batchnorm/add_1")
-                                    .run()
-                                    .get(0)
-                                    .expect(Float.class);
-      float[][] embedding = new float[1][(int) output.shape()[1]];
-      output.copyTo(embedding);
-      return embedding[0];
-    } catch (Exception e) {
-      e.printStackTrace();
+    public float[] getFaceEmbedding(Mat face) {  
+        float[] embedding = null;  
+        try (Tensor<Float> tensor = normalizeImage(face)) {  
+            Tensor<Float> output = session.runner()  
+                    .feed("input", tensor)  
+                    .fetch("Bottleneck_BatchNorm/batchnorm/add_1")  
+                    .run()  
+                    .get(0)  
+                    .expect(Float.class);  
+            embedding = new float[(int) output.shape()[1]];  
+            output.copyTo(embedding);  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+        return embedding;  
+    }  
+  
+    private Tensor<Float> normalizeImage(Mat mat) {  
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGB);  
+        mat.convertTo(mat, CvType.CV_32F);
+        FloatBuffer fb = convertMatToFloatBuffer(mat); 
+        return Tensor.create(new long[]{1, 160, 160, 3}, fb);  
+    } 
+
+  public static FloatBuffer convertMatToFloatBuffer(Mat mat) {
+        int bufferSize = (int) (mat.total() * mat.channels());
+        FloatBuffer floatBuffer = ByteBuffer.allocateDirect(bufferSize * Float.BYTES).asFloatBuffer();
+        float[] data = new float[bufferSize];
+        mat.get(0, 0, data);
+        floatBuffer.put(data);
+        floatBuffer.rewind();
+        return floatBuffer;
     }
-    return null;
-  }
-
-  private Mat normalizeImage(Mat mat) {
-    Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGB);
-    mat.convertTo(mat, CvType.CV_32F);
-    Core.divide(mat, Scalar.all(255.0), mat);
-    return mat;
-  }
 }
